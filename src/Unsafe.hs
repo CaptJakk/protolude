5aedfa4b0cfedbfdecffa62e83564c9ae7d13931
Keagan McClelland <kmcclel2@illinois.edu>
2019-07-28 19:13:17 -0600

    adds missing unsafeFromRight

--- src/Unsafe.hs
+++ src/Unsafe.hs
@@ -12,6 +12,7 @@ module Unsafe (
 ) where
 
 import Protolude.Base (Int)
+import qualified Data.Either as Either
 import qualified Data.List as List
 import qualified Data.Maybe as Maybe
 import qualified Control.Exception as Exc
@@ -30,6 +31,9 @@ unsafeLast = List.last
 
 unsafeFromJust :: Maybe.Maybe a -> a
 unsafeFromJust = Maybe.fromJust
+
+unsafeFromRight :: Either.Either e a -> a
+unsafeFromRight (Right a) = a
 
 unsafeIndex :: [a] -> Int -> a
 unsafeIndex = (List.!!)
